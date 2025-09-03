9323c97b5e474f9c7070dc2e739e4af70ed39b41
vijay <vijaykrishna4941@gmail.com>
2025-09-03 11:00:52 +0530

    Initial Commit

---
+++ UserList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UserList
+//
+//  Created by vijay krishna on 03/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UserList/UserListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserListApp.swift
+//  UserList
+//
+//  Created by vijay krishna on 03/09/25.
+//
+
+import SwiftUI
+
+@main
+struct UserListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
